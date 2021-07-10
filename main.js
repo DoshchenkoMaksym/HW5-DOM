c25891d9c6664c428b27ae7397bbc43589597319
Maksym Doshchenko <doschenko54@gmail.com>
2021-07-10 10:47:57 +0300

    fixed js code

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-let buttonSubmit = document.getElementsByClassName('form__inner-submit');
-let inputName = document.getElementsByClassName('form__inner-name');
-let inputPassword = document.getElementsByClassName('form__inner-password');
+let buttonSubmit = document.getElementById('submit');
+let inputName = document.getElementById('input-name');
+let inputPassword = document.getElementById('input-password');
 let resultName = document.getElementsByClassName('result__name');
 let resultPassword = document.getElementsByClassName('result__password');
 let resultClass = document.getElementsByClassName('result');
+
 function local() {
-    localStorage.setItem('name', inputName[0].value);
-    localStorage.setItem('password', inputPassword[0].value);
+    localStorage.setItem('name', inputName.value);
+    localStorage.setItem('password', inputPassword.value);
 };
+
 function show() {
-    if(localStorage.name && localStorage.password){
+    if (localStorage.name && localStorage.password) {
         resultClass[0].classList.add("result__down")
-        resultName[0].innerHTML = `Пользователь ввёл имя: ${localStorage.getItem('name')}`;;
+        resultName[0].innerHTML = `Пользователь ввёл имя: ${localStorage.getItem('name')}`;
         resultPassword[0].innerHTML = `Пользователь ввёл пароль: ${localStorage.getItem('password')}`;
-    } else if(!localStorage.name && !localStorage.password) {
-        resultClass[0].classList.remove("result__down")
+    } else if (!localStorage.name && !localStorage.password) {
+        resultClass[0].classList.remove("result__down");
     };
-    
 };
-buttonSubmit[0].addEventListener("click", () => {
-    local(inputName, inputPassword)
-});
-buttonSubmit[0].addEventListener("click", () => {
-    show(resultName, resultPassword)
+
+buttonSubmit.addEventListener("click", () => {
+    local(inputName, inputPassword);
+    show(resultName, resultPassword);
 });
 
